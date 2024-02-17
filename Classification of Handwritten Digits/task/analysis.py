8efbd540d3d51087ca72a973c678812e0bce80f4
radosan <radoslaw.sanetra@gmail.com>
2024-02-17 12:12:52 +0100

    split to train and tes sets

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -1,14 +1,19 @@
 from keras.datasets.mnist import load_data
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def main():
-    (x_train, y_train), test = load_data()
-    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
-    print("Classes:", np.unique(y_train))
-    print("Features' shape:", x_train.shape)
-    print("Target's shape:", y_train.shape)
-    print(f"min: {np.min(x_train)}, max: {np.max(x_train)}")
+    (x, y), _ = load_data()
+    x = x.reshape(x.shape[0], -1)
+    x_train, x_test, y_train, y_test = train_test_split(x[:6000], y[:6000], test_size=0.3, random_state=40)
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+    print('y_train shape:', y_train.shape)
+    print('y_test shape:', y_test.shape)
+    print('Proportion of samples per class in train set:')
+    print(pd.Series(y_train).value_counts(normalize=True))
 
 
 if __name__ == '__main__':
