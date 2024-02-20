a5f404beb1741bf23b0bd172921233b3449cd9a5
radosan <radoslaw.sanetra@gmail.com>
2024-02-20 19:41:19 +0100

    added models testing

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -1,19 +1,32 @@
 from keras.datasets.mnist import load_data
-import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+
+def fit_predict_eval(model, x_train, x_test, y_train, y_test, verbose=True):
+    model.fit(x_train, y_train)
+    prediction = model.predict(x_test)
+    score = accuracy_score(y_test, prediction)
+    if verbose:
+        print(f'Model: {model}\nAccuracy: {score}\n')
+    return score
 
 
 def main():
     (x, y), _ = load_data()
     x = x.reshape(x.shape[0], -1)
     x_train, x_test, y_train, y_test = train_test_split(x[:6000], y[:6000], test_size=0.3, random_state=40)
-    print('x_train shape:', x_train.shape)
-    print('x_test shape:', x_test.shape)
-    print('y_train shape:', y_train.shape)
-    print('y_test shape:', y_test.shape)
-    print('Proportion of samples per class in train set:')
-    print(pd.Series(y_train).value_counts(normalize=True))
+
+    models = [KNeighborsClassifier(), DecisionTreeClassifier(random_state=40),
+              LogisticRegression(random_state=40), RandomForestClassifier(random_state=40)]
+    scores = {}
+    for model in models:
+        scores[type(model).__name__] = fit_predict_eval(model, x_train, x_test, y_train, y_test)
+    print(f'The answer to the question: {max(scores)} - {scores[max(scores)]:.3f}')
 
 
 if __name__ == '__main__':
